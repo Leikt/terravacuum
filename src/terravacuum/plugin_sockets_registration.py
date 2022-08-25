40774fd747729ca6678fc832694ff67a004e551c
rlioret <robin.lioret@capgemini.com>
2022-08-25 15:54:38 +0200

    Add comment

--- src/terravacuum/plugin_sockets_registration.py
+++ src/terravacuum/plugin_sockets_registration.py
@@ -1,4 +1,5 @@
 def register_plugin_sockets():
+    """Initialize the plugin sockets. Please call this before loading any plugin."""
     from .plugin_loader import PluginLoader
 
     from .file_loader import FileLoaderPluginSocket
